74c4168992d2e2c9afe4aa2d8bde894750ad07cc
jurdan21 <jurdan.aho@gmail.com>
2025-07-25 21:42:51 +0700

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
         <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         {/* Hapus semua link favicon eksternal, biarkan Next.js ambil favicon.ico dari /public */}
         <meta name="google-site-verification" content="bejPZAYfp_9b7ogV1JyFmfr9yTGuy97F8ViQ3swi614" />
         <meta name="description" content="Freebieskit is your source for free UI kits, mockups, icons, illustrations, templates, and more. Discover and download high-quality design resources for web, mobile, and creative projects. Curated for designers, developers, and creators." />
