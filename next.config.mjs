ab9c70afc59ddc411bef5a586e55a0af7144e891
jurdan21 <jurdan.aho@gmail.com>
2025-07-27 19:31:33 +0700

    fix error index

--- next.config.mjs
+++ next.config.mjs
@@ -2,35 +2,6 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.freebieskit.com',
-          },
-        ],
-        destination: 'https://freebieskit.com/:path*',
-        permanent: true,
-        statusCode: 301,
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Robots-Tag',
-            value: 'index, follow',
-          },
-        ],
-      },
-    ];
-  },
   trailingSlash: false,
 };
 
