826c2efca3a570e325ca9f38e47d2a7d15602c17
jurdan21 <jurdan.aho@gmail.com>
2025-07-27 19:21:36 +0700

    fix index

--- next.config.mjs
+++ next.config.mjs
@@ -14,9 +14,24 @@ const nextConfig = {
         ],
         destination: 'https://freebieskit.com/:path*',
         permanent: true,
+        statusCode: 301,
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow',
+          },
+        ],
+      },
+    ];
+  },
+  trailingSlash: false,
 };
 
 export default nextConfig;
